dcbfeeddc53b6cea47ba7fdd5002589a8bc14db5
Mikul Gohil <mikulgohil@gmail.com>
2014-06-30 14:23:03 +0530

    Setup

---
+++ app/_posts/2014-06-19-yo-jekyllrb.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title:  "Yo Jekyll!"
+categories: jekyll update
+---
+
+# 'Allo, 'Allo!
+
+You now have
+
+- Jekyll
+- rdiscount markdown parser
+
+installed.
+
+**Enjoy coding!**
---
+++ app/scripts/modern-business.js
@@ -0,0 +1,13 @@
+'use strict';
+$(document).ready(function() {
+    console.log('hello');
+    // Activates the Carousel
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+    // Activates Tooltips for Social Links
+    $('.tooltip-social').tooltip({
+        selector: 'a[data-toggle=tooltip]'
+    });
+});
